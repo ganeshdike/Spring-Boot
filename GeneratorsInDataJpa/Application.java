3dfd9edaa18f67350cadee2cf5874c6c37146fcf
ganeshdike <ganeshdike4636@gmail.com>
2023-02-22 08:43:17 +0530

    Small Changes

--- GeneratorsInDataJpa/Application.java
+++ GeneratorsInDataJpa/Application.java
@@ -8,7 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort ;
 
 import java.util.ArrayList;
 import java.util.Arrays;
